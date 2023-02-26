73d2e85fa083df3f9bd9eedf603a66b598063e54
Danz <DvRossen@outlook.com>
2023-02-26 22:42:49 +0100

    loading in img

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", init);
-const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
-
 function init() {
   intro = document.getElementById("intro");
   gameDiv = document.getElementById("game");
   startButton = document.getElementById("start");
   howToPlayButton = document.getElementById("how-to-play");
   reloadButton = document.getElementById("reload");
+  img = document.getElementById("output");
+  input = document.getElementById("file");
 
   startButton.addEventListener("click", start);
   howToPlayButton.addEventListener("click", htplay);
@@ -14,9 +14,11 @@ function init() {
 
   game.style.display = "none";
 }
+const featureExtractor = ml5.featureExtractor("MobileNet", modelLoaded);
+
 function start() {
   intro.style.display = "none";
-  game.style.display = "block";
+  gameDiv.style.display = "block";
 }
 
 function htplay() {
@@ -25,6 +27,20 @@ function htplay() {
   );
 }
 
+function fileAdded() {
+  input.style.display = "none";
+  img.src = URL.createObjectURL(event.target.files[0]);
+  img.style.display = "inline-block";
+  classify();
+  retrybtn.style.display = "inline-block";
+}
+
 function reload() {
+  img.style.display = "none";
+  input.value = null;
+  input.style.display = "inline-block";
+  console.log(img.src);
+  URL.revokeObjectURL(img.src);
+  img.src = "";
   location.reload();
 }
